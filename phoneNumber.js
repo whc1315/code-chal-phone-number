81a00ac5b7fb48c1ee5f2528444c48bf7e7332a2
William Cuthbertson <whc1315@gmail.com>
2022-02-01 08:58:21 -0700

    Challenge done

---
+++ phoneNumber.js
@@ -0,0 +1,6 @@
+const createPhoneNumber = (arr) => {
+  console.log(
+    `(${arr[0]}${arr[1]}${arr[2]})-${arr[3]}${arr[4]}${arr[5]}-${arr[6]}${arr[7]}${arr[8]}${arr[9]}`
+  );
+};
+createPhoneNumber([6, 2, 3, 2, 5, 8, 1, 0, 6, 8]);
